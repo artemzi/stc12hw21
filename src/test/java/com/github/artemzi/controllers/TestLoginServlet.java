4339ef78594afa5c3a88df8ea23fc88c1aed4141
Artem Z <arzinoviev@gmail.com>
2018-10-06 16:28:43 +0300

    Clean up imports

--- src/test/java/com/github/artemzi/controllers/TestLoginServlet.java
+++ src/test/java/com/github/artemzi/controllers/TestLoginServlet.java
@@ -11,12 +11,8 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class TestLoginServlet {
